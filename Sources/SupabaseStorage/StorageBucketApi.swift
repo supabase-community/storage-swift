feeb6fb05701e30e685103d4e014a5af9b0a9bd1
Mathieu Barnachon <mathieu@styleshoots.com>
2022-02-23 10:50:05 +0100

    Add Linux compatibility (#4)

--- Sources/SupabaseStorage/StorageBucketApi.swift
+++ Sources/SupabaseStorage/StorageBucketApi.swift
@@ -1,4 +1,7 @@
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 /// Storage Bucket API
 public class StorageBucketApi: StorageApi {
--- Sources/SupabaseStorage/StorageFileApi.swift
+++ Sources/SupabaseStorage/StorageFileApi.swift
@@ -1,4 +1,7 @@
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 /// Supabase Storage File API
 public class StorageFileApi: StorageApi {
