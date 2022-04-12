ebb1ee7370ec0d50bcfb38ce05279e1475aebbf8
Cosmin Anghel <cosminzorr@gmail.com>
2022-04-13 02:45:46 +0300

    Make StorageError initializer public (#5)

--- Sources/SupabaseStorage/StorageError.swift
+++ Sources/SupabaseStorage/StorageError.swift
@@ -3,6 +3,11 @@ import Foundation
 public struct StorageError: Error {
   public var statusCode: Int?
   public var message: String?
+      
+  public init(statusCode: Int? = nil, message: String? = nil) {
+    self.statusCode = statusCode
+    self.message = message
+  }
 }
 
 extension StorageError: LocalizedError {
