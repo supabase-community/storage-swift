4789d7d8f9043725003b0a0c82315b6f484695ae
Guilherme Souza <grsouza@pm.me>
2022-10-21 06:36:07 -0300

    Rename FileObject properties to camel case

--- Sources/SupabaseStorage/FileObject.swift
+++ Sources/SupabaseStorage/FileObject.swift
@@ -3,9 +3,9 @@ public struct FileObject {
   public var bucket_id: String
   public var owner: String
   public var id: String
-  public var updated_at: String
-  public var created_at: String
-  public var last_accessed_at: String
+  public var updatedAt: String
+  public var createdAt: String
+  public var lastAccessedAt: String
   public var metadata: [String: Any]
   public var buckets: Bucket?
 
@@ -15,9 +15,9 @@ public struct FileObject {
       let bucket_id = dictionary["bucket_id"] as? String,
       let owner = dictionary["owner"] as? String,
       let id = dictionary["id"] as? String,
-      let updated_at = dictionary["updated_at"] as? String,
-      let created_at = dictionary["created_at"] as? String,
-      let last_accessed_at = dictionary["last_accessed_at"] as? String,
+      let updatedAt = dictionary["updated_at"] as? String,
+      let createdAt = dictionary["created_at"] as? String,
+      let lastAccessedAt = dictionary["last_accessed_at"] as? String,
       let metadata = dictionary["metadata"] as? [String: Any],
       let buckets = dictionary["buckets"] as? [String: Any]
     else {
@@ -28,9 +28,9 @@ public struct FileObject {
     self.bucket_id = bucket_id
     self.owner = owner
     self.id = id
-    self.updated_at = updated_at
-    self.created_at = created_at
-    self.last_accessed_at = last_accessed_at
+    self.updatedAt = updatedAt
+    self.createdAt = createdAt
+    self.lastAccessedAt = lastAccessedAt
     self.metadata = metadata
     self.buckets = Bucket(from: buckets)
   }
